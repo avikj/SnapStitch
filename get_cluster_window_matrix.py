58c6f1181acbf954431c5b20a7dfc5ec8f08047d
Anish Nag <anishnag@Anishs-MacBook-Pro.local>
2017-07-30 09:25:54 -0700

    sort frames

--- get_cluster_window_matrix.py
+++ get_cluster_window_matrix.py
@@ -103,11 +103,8 @@ def get_best_seqs(projid, window_size=30):
 	vid_seq_dict = {k:[] for k in set([seq[0] for seq in best_seqs])}
 	for seq in best_seqs: vid_seq_dict[seq[0]].append(seq[1])
 	print '\n'
-	print vid_seq_dict
-
 	for vid in vid_seq_dict.keys():
 		vid_seq_dict[vid].sort(key=lambda x: x[0])
-
 	print vid_seq_dict
 
 	for vid in vid_seq_dict.keys():
