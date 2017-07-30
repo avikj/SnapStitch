6ba8c64b71de1276ef5a0e7aef2868c846b31f0a
Anish Nag <anishnag@Anishs-MacBook-Pro.local>
2017-07-29 22:09:21 -0700

    untested matrix analysis

--- get_cluster_window_matrix.py
+++ get_cluster_window_matrix.py
@@ -1,68 +1,73 @@
 # get_cluster_window_matrix.py
+
 import numpy as np
 import pickle
+import sys
 
-FRAME_PATH = '/frames/...'
-SLIDING_WINDOW_SIZE = 30
-
-# cluster_filename = 'test.pickle'
-# t = (2, {'frame1.jpg':0,'frame2.jpg':0,'frame3.jpg':1,'frame4.jpg':1})
-
-with open(cluster_filename, 'wb') as stream:
-	pickle.dump(t, stream)
-
-def get_filenames_to_clusters(filename):
-	with open(filename, 'rb') as stream:
-		data = pickle.load(stream)
-	return data
-
-# returns dictionary with keys of cluster numbers and values of frames
-def get_cluster_to_filenames(filenames_to_cluster):
-	num_clusts = np.max(np.array([clust for clust in filenames_to_cluster.values()])) + 1
-	cluster_to_filenames = {k:[] for k in range(num_clusts)}
-	for frame, clust in filenames_to_cluster.iteritems():
+def get_clust_to_filename(filename_to_clust):
+	num_clusts = max([clust for clust in filename_to_clust.values()]) + 1
+	clust_to_filename = {k:[] for k in range(num_clusts)}
+	for frame, clust in filename_to_clust.iteritems():
 		if clust == -1: continue
-		cluster_to_filenames[clust].append(frame)
-	return cluster_to_filenames
+		clust_to_filename[clust].append(frame)
+	return clust_to_filename
 
-def get_frame_vector(frame):
-	path = FRAME_PATH + frame + '.npy'
-	return np.load(path)
+def get_embed(frame_img):
+	return np.load(os.path.join(frame_img[:-20], 'embeddings', filename[-14:-4] + '.npy'))
 
-def get_centroid(cluster):
-	clust_points = np.array([get_frame_vector(frame) for frame in clust])
-	assert(clust_points.shape[0] == len(cluster) and clust_points[1] == 2048)
+def get_centroid(clust):
+	clust_points = np.array([get_embed(frame) for frame in clust])
 	centroid = np.mean(clust_points, axis=0)
 	return centroid
 
-def get_avg_error(window, centroid):
-	frames = np.array([get_frame_vector(frame) for frame in window])
-	assert(frames.shape[0] == len(window) and frames.shape[1] == 2048)
-	diffs = frames - centroid
-	errors = np.linalg.norm(diffs, ord=2, axis=1)
-	avg_error = np.mean(dists)
-	return avg_error
-	
-def get_best_windows(cluster_to_filenames):
-	# compute matrix of window vs. cluster
+def get_error(win, centroid):
+	embeds = np.array([get_embed(frame) for frame in win])
+	diffs = embeds - centroid
+	errors = np.linalg(diffs, ord=2, axis=1)
+	return np.mean(errors)
+
+def compute_win_clust_mat(filename_to_clust, window_size=30):
+	clust_to_filename = get_clust_to_filename(filename_to_clust)
+	video_wins = []
 	win_clust_vecs = []
-	for clust in cluster_to_filenames.keys():
-		filenames = cluster_to_filenames[clust]
+	for clust in clust_to_filename.keys():
+		filenames = clust_to_filename[clust]
 		centroid = get_centroid(filenames)
-		file_windows = [filenames[i:i+SLIDING_WINDOW_SIZE] for i in xrange(len(filenames)-SLIDING_WINDOW_SIZE+1)]
-		clust_errors = np.array([get_avg_error(window, centroid) for window in file_windows])
-		win_clust_vecs.append(clust_errors)
-	win_clust_mat = np.vstack(np.array(win_clust_vecs))
-	assert(win_clust_mat.shape[1] == len(cluster_to_filenames.keys()))
+		wins = [filenames[i:i+window_size] for i in xrange(len(filenames)-window_size+1)]
+		errors = np.array([get_error(win, centroid) for win in wins])
+		video_wins.append(wins)
+		win_clust_vecs.append(errors)
+	win_clust_mat = np.vstack(np.array(win_clust_vecs)) # TODO: check if vstack or hstack
+	return (win_clust_mat, video_wins)
 
-	# find best index of best window
-	best_wins = np.argmax(win_clust_mat, axis=0)
-	return best_wins
+def get_best_seqs(projid, window_size=30):	
+	# load clustered frames
+	with open(os.path.join('static', projid, 'filename_to_clust.pkl'), 'rb') as stream:
+		filename_to_clust = pickle.load(stream)
 
+	# contruct matrix for every video
+	video_names = filename_to_clust.keys()
+	mat_wins = [compute_win_clust_mat(video_clustered_frames, window_size) for video_clustered_frames in filename_to_clust.values()]
+	matrices = [mat_win[0] for mat_win in mat_wins]
+	wins = [[(win[0], win[-1]) for win in mat_win[1]] for mat_win in mat_wins] # contructs tuples of start and end frames
+	flattened_wins = [win for win in vid_wins for vid_wins in wins] 
 
-# get clustered frames
-filenames_to_cluster = get_filenames_to_clusters(cluster_filename)
-cluster_to_filenames = get_cluster_to_filenames(filenames_to_cluster)
+	# get first indices of videos
+	index = 0
+	indices = [0]
+	for x in xrange(len(wins)):
+		index += len(wins[x])
+		indices.append(index)
 
-# get best windows
-best_wins = get_best_windows(cluster_to_filenames)
+	# concatenate matrices & find best sequences
+	mat = np.concatenate(matrices, axis=0)
+	argmins = np.argmin(mat, axis=0)
+	vid_indices = [bisect.bisect(indices, argmin)-1 for argmin in argmins]
+	best_seqs = [(video_names[vid_indices[x]], flattened_wins[argmins[x]]) for x in xrange(len(wins))]
+	return best_seqs
+
+if __name__ == "__main__":
+	projid = sys.argv[1]
+	# window_size
+	seqs = get_best_seqs(projid)
+	print seqs
